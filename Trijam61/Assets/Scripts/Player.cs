1ad59e6c72b7c69eee13fb06290b45fc7966ec3a
Team-on <timonsol13@gmail.com>
2020-03-15 05:38:15 +0200

    Die logic

--- Trijam61/Assets/Scripts/Player.cs
+++ Trijam61/Assets/Scripts/Player.cs
@@ -2,17 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour {
+	[Header("Moving")]
 	[SerializeField] float moveSpeed = 5.0f;
 	[SerializeField] float jumpForce = 20.0f;
 	[SerializeField] LayerMask groundMask;
 	[SerializeField] Transform groundCheck;
 
+	[Header("Worlds")]
 	[SerializeField] GameObject[] worlds;
 	byte currWorld = 0;
+
+	[Header("Die")]
+	[SerializeField] Transform aroundCanvas;
+	[SerializeField] Transform respawnPoint;
+	[SerializeField] DieData[] dieDatas;
+	byte currDieDialog = 0;
 
 	[HideInInspector] [SerializeField] Rigidbody2D rb;
 	[HideInInspector] [SerializeField] Animator anim;
@@ -21,6 +30,7 @@ public class Player : MonoBehaviour {
 	Vector3 m_Velocity = Vector3.zero;
 	bool isGrounded = true;
 	bool isFacingRight = true;
+	bool isCanControl = true;
 
 	void OnValidate() {
 		if(rb == null)
@@ -29,20 +39,21 @@ public class Player : MonoBehaviour {
 			anim = GetComponent<Animator>();
 	}
 
-	//private void OnCollisionEnter2D(Collision2D collision) {
-	//	if (collision.transform.tag == "Floor") {
-	//		isGrounded = true;
-	//	}
-	//}
-
 	void Update() {
 		moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		if (!isCanControl) 
+			return;
 
 		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.K)) {
 			worlds[currWorld].SetActive(false);
 			if (++currWorld >= worlds.Length)
 				currWorld = 0;
 			worlds[currWorld].SetActive(true);
+		}
+
+		if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.K)) {
+			Die();
 		}
 	}
 
@@ -52,22 +63,65 @@ public class Player : MonoBehaviour {
 		for (int i = 0; i < colliders.Length; i++) {
 			if (colliders[i].gameObject != gameObject) {
 				isGrounded = true;
+				isCanControl = true;
 			}
 		}
 
 		Move(moveInput);
 	}
 
-	private void Move(Vector2 direction) {
-		Vector3 targetVelocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
-		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, .05f);
+	public void Die() {
+		transform.position = respawnPoint.position;
+		isCanControl = false;
+		rb.gravityScale = 0.0f;
+		rb.velocity = Vector3.zero;
+		DieData dieData = dieDatas[currDieDialog];
 
+
+		float allTime = 0.0f;
+		for(byte i = 0; i < dieData.time.Length; ++i) {
+			int curr = i;
+			LeanTween.delayedCall(allTime, () => {
+				LeanTween.cancel(dieData.dialog.gameObject);
+				dieData.texts[curr].gameObject.SetActive(true);
+				if(curr != 0)
+					dieData.texts[curr - 1].gameObject.SetActive(false);
+				dieData.dialog.localPosition = Vector3.zero;
+				LeanTween.moveLocalY(dieData.dialog.gameObject, dieData.time[curr] * dieData.speed[curr], dieData.time[curr]);
+			});
+
+			allTime += dieData.time[i];
+		}
+
+		LeanTween.delayedCall(allTime, () => {
+			dieData.texts[dieData.time.Length - 1].gameObject.SetActive(false);
+			rb.gravityScale = 1.0f;
+			if(currDieDialog != dieDatas.Length - 1) {
+				++currDieDialog;
+			}
+			else {
+				dieData.texts[0].text += "?";
+			}
+		});
+
+	}
+
+	private void Move(Vector2 direction) {
 		if (direction.x > 0 && !isFacingRight) {
 			Flip();
 		}
 		else if (direction.x < 0 && isFacingRight) {
 			Flip();
 		}
+
+		if (!isCanControl) {
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, new Vector2(0, rb.velocity.y), ref m_Velocity, .05f);
+			anim.SetBool("IsMoving", moveInput != Vector2.zero);
+			return;
+		}
+
+		Vector3 targetVelocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
+		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, .05f);
 
 		if (isGrounded && direction.y >= 0.5f) {
 			isGrounded = false;
@@ -88,5 +142,17 @@ public class Player : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+
+		theScale = aroundCanvas.localScale;
+		theScale.x *= -1;
+		aroundCanvas.localScale = theScale;
 	}
+}
+
+[Serializable]
+struct DieData {
+	public Transform dialog;
+	public TextMeshProUGUI[] texts;
+	public float[] time;
+	public float[] speed;
 }
