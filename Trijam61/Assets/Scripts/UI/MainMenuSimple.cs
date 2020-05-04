c6c5e98314c7766c8c778c78ed50c779a0f8a75a
Team <timonsol13@gmail.com>
2020-05-05 02:59:47 +0300

    Better main menu

--- Trijam61/Assets/Scripts/UI/MainMenuSimple.cs
+++ Trijam61/Assets/Scripts/UI/MainMenuSimple.cs
@@ -1,13 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using NaughtyAttributes;
 
 public class MainMenuSimple : MonoBehaviour {
 	[SerializeField] CanvasGroup canvasGroup;
+	[Space]
+	[SerializeField] Player player;
+	[Space]
+	[SerializeField] string[] layerMaskMenu;
+	[SerializeField] string[] layerMaskGame;
+	[SerializeField] Camera camera;
+	[SerializeField] CinemachineVirtualCamera menuCamera;
+	[SerializeField] CinemachineVirtualCamera gameCamera;
+
+	private void Awake() {
+		menuCamera.enabled = true;
+		gameCamera.enabled = false;
+		camera.cullingMask = LayerMask.GetMask(layerMaskMenu);
+	}
 
 	public void OnPlayClick() {
 		canvasGroup.interactable = false;
 		canvasGroup.blocksRaycasts = true;
 		LeanTweenEx.ChangeCanvasGroupAlpha(canvasGroup, 0.0f, 0.25f);
+
+		menuCamera.enabled = false;
+		gameCamera.enabled = true;
+		camera.cullingMask = LayerMask.GetMask(layerMaskGame);
+	}
+
+	public void OnLevelsClick() {
+
+	}
+
+	public void OnSettingsClick() {
+
 	}
 }
--- Trijam61/Assets/Scripts/UI/Menu/MainMenu.cs
+++ Trijam61/Assets/Scripts/UI/Menu/MainMenu.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MenuBase {
-	public void Play() {
-		SceneLoader.Instance.LoadScene("SampleScene2D", true, true);
-
+	public void OnPlayClick() {
+		canvasGroup.interactable = false;
+		canvasGroup.blocksRaycasts = true;
+		LeanTweenEx.ChangeCanvasGroupAlpha(canvasGroup, 0.0f, 0.25f);
 	}
 
-	public void Load() {
-		SceneLoader.Instance.LoadScene("SampleScene2D", true, true);
+	public void OnLevelsClick() {
+		canvasGroup.interactable = false;
+		canvasGroup.blocksRaycasts = true;
+		LeanTweenEx.ChangeCanvasGroupAlpha(canvasGroup, 0.0f, 0.25f);
+	}
+
+	public void OnSettingsClick() {
+		canvasGroup.interactable = false;
+		canvasGroup.blocksRaycasts = true;
+		LeanTweenEx.ChangeCanvasGroupAlpha(canvasGroup, 0.0f, 0.25f);
 	}
 }
