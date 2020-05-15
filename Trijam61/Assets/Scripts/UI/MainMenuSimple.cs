eca027ffff5eca4e991de14f469d37774705fa53
Team <timonsol13@gmail.com>
2020-05-15 15:31:50 +0300

    Change space to linear

--- Trijam61/Assets/Scripts/UI/MainMenuSimple.cs
+++ Trijam61/Assets/Scripts/UI/MainMenuSimple.cs
@@ -28,20 +28,30 @@ public class MainMenuSimple : MonoBehaviour {
 	[Header("Post Processing")]
 	[Space]
 	[SerializeField] PostProcessVolume postProcessVolume;
+	[SerializeField] [ColorUsage(false, true)] Color bloomColorPositive;
+	[SerializeField] [ColorUsage(false, true)] Color bloomColorNegative;
 	Grain grain;
 	LensDistortion lensDistortion;
+	ChromaticAberration chromaticAberration;
+	Bloom bloom;
 
 	float menuDefaultAlpha;
 	float defaultLensDistortionIntensity;
 	float defaultGrainIntensity;
+	float defaultChromaticAberrationIntensity;
 
 	private void Awake() {
 		postProcessVolume.profile.TryGetSettings(out grain);
 		postProcessVolume.profile.TryGetSettings(out lensDistortion);
+		postProcessVolume.profile.TryGetSettings(out chromaticAberration);
+		postProcessVolume.profile.TryGetSettings(out bloom);
 
 		menuDefaultAlpha = canvasGroup.alpha;
 		defaultLensDistortionIntensity = lensDistortion.intensity.value;
 		defaultGrainIntensity = grain.intensity.value;
+		defaultChromaticAberrationIntensity = chromaticAberration.intensity.value;
+
+		bloom.color.value = bloomColorPositive;
 	}
 
 	private void Start() {
@@ -84,6 +94,7 @@ public class MainMenuSimple : MonoBehaviour {
 
 		if (isForce) {
 			grain.intensity.value = defaultGrainIntensity;
+			chromaticAberration.intensity.value = 1.0f;
 			canvasGroup.alpha = menuDefaultAlpha;
 			player.ChangeLevelTextAlpha(0.0f, 0.0f, true);
 			virtualCamera.m_Lens.OrthographicSize = 2;
@@ -92,6 +103,11 @@ public class MainMenuSimple : MonoBehaviour {
 			LeanTween.value(gameObject, grain.intensity.value, defaultGrainIntensity, 0.33f)
 			.setOnUpdate((float intensity) => {
 				grain.intensity.value = intensity;
+			});
+
+			LeanTween.value(gameObject, chromaticAberration.intensity.value, 1.0f, 0.33f)
+			.setOnUpdate((float intensity) => {
+				chromaticAberration.intensity.value = intensity;
 			});
 
 			LeanTweenEx.ChangeCanvasGroupAlpha(gameObject, canvasGroup, menuDefaultAlpha, 0.33f);
@@ -111,7 +127,7 @@ public class MainMenuSimple : MonoBehaviour {
 	public void HideGameMenu() {
 		LeanTween.cancel(gameObject, false);
 		player.StopChangeLevelTextAlpha();
-		isInMenu = false;
+		LeanTweenEx.InvokeNextFrame(exitButton.gameObject, () => isInMenu = false);
 		
 		canvasGroup.interactable = false;
 		canvasGroup.blocksRaycasts = false;
@@ -120,6 +136,11 @@ public class MainMenuSimple : MonoBehaviour {
 		LeanTween.value(gameObject, virtualCamera.m_Lens.OrthographicSize, 5.0f, 1.0f)
 		.setOnUpdate((float size) => { 
 			virtualCamera.m_Lens.OrthographicSize = size;
+		});
+
+		LeanTween.value(gameObject, chromaticAberration.intensity.value, defaultChromaticAberrationIntensity, 0.33f)
+		.setOnUpdate((float intensity) => {
+			chromaticAberration.intensity.value = intensity;
 		});
 
 		LeanTween.value(gameObject, grain.intensity.value, 0, 0.33f)
@@ -150,24 +171,36 @@ public class MainMenuSimple : MonoBehaviour {
 	public void ToNormalWorldLens(float time, bool isForce) {
 		if (isForce) {
 				lensDistortion.intensity.value = defaultLensDistortionIntensity;
+				bloom.color.value = bloomColorPositive;
 			return;
 		}
 
-		LeanTween.value(lensDistortion.intensity.value, defaultLensDistortionIntensity, time)
-			.setOnUpdate((float intensity) => {
-				lensDistortion.intensity.value = intensity;
-			});
+		LeanTween.value(gameObject, lensDistortion.intensity.value, defaultLensDistortionIntensity, time)
+		.setOnUpdate((float intensity) => {
+			lensDistortion.intensity.value = intensity;
+		});
+
+		LeanTween.value(gameObject, bloom.color.value, bloomColorPositive, time)
+		.setOnUpdate((Color c) => {
+			bloom.color.value = c;
+		});
 	}
 
 	public void ToNegativeWorldLens(float time, bool isForce) {
 		if (isForce) {
 			lensDistortion.intensity.value = -defaultLensDistortionIntensity;
+				bloom.color.value = bloomColorNegative;
 			return;
 		}
 
-		LeanTween.value(lensDistortion.intensity.value, -defaultLensDistortionIntensity, time)
-			.setOnUpdate((float intensity) => {
-				lensDistortion.intensity.value = intensity;
-			});
+		LeanTween.value(gameObject, lensDistortion.intensity.value, -defaultLensDistortionIntensity, time)
+		.setOnUpdate((float intensity) => {
+			lensDistortion.intensity.value = intensity;
+		});
+
+		LeanTween.value(gameObject, bloom.color.value, bloomColorNegative, time)
+		.setOnUpdate((Color c) => {
+			bloom.color.value = c;
+		});
 	}
 }
